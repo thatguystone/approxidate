a3a7eba95fe7d46fe8336ed3dd897a9c90ed23a9
Andrew Stone <andrew@clovar.com>
2013-04-10 17:46:16 -0400

    Removing that stupid debug statement

--- python/pyapproxidate.pyx
+++ python/pyapproxidate.pyx
@@ -3,5 +3,4 @@ cimport capproxidate
 def approx(char *date):
 	cdef capproxidate.timeval tv
 	capproxidate.approxidate(date, &tv)
-	print tv.tv_sec, tv.tv_usec
 	return tv.tv_sec + (tv.tv_usec / 1000000.0)
