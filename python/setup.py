d6b46a9e24434f6e6452e1c868ae4ab4474f097e
Andrew Stone <a@stoney.io>
2015-07-28 15:10:01 -0400

    Update email

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ setup(
 	version='1.0.3',
 	url='https://github.com/thatguystone/approxidate',
 	author='Andrew Stone',
-	author_email='andrew@clovar.com',
+	author_email='a@stoney.io',
 	license='GPL v2',
 	description='a quick way to get a unix timestamp from a ton of arbitrary date formats',
 	long_description=long_description,
